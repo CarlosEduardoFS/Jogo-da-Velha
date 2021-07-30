da2b9d9101d2fdba573f6db72f6ff24021e4a6d5
CarlosEduardoFS <eduardofs_2016@outlook.com>
2021-07-30 16:47:29 -0300

    Ajustes

--- Jogo da Velha/main.c
+++ Jogo da Velha/main.c
@@ -10,21 +10,24 @@
 *
 */
 
-// Declaração de um registro 
+// Declaraï¿½ï¿½o de um registro 
 typedef struct {
 	
 	char nome[20];
-	char simbolo;
+	int simbolo;
 	
 }dados_jogadores;
 
 
-void definicao_posicoes(char posicoes[3][3]);
+void definicao_posicoes(int posicoes[3][3]);
 void cadastro_jogadores(dados_jogadores *jogadores);
-int tabuleiro(char posicoes[3][3], int exibicao_necessaria);
-int jogada(char posicoes[3][3], int posicao_jogada,dados_jogadores *dados,int jogador);
-int posicao_valida(char posicoes[3][3], int posi);
-int verificacao_gahador(char posicoes[3][3]);
+void preencher_tabuleiro_vizualizacao(int posicoes[3][3], char vizualizacao[3][3]);
+int* posicao_tabuleiro(int posicao);
+void tabuleiro(int tabuleiro_posi[3][3]);
+int seleciona_posicao();
+int jogada(int posicoes[3][3], int* posicao_jogada,dados_jogadores *dados,int jogador);
+int posicao_valida(int posicoes[3][3], int* posi);
+int verificacao_gahador(int posicoes[3][3]);
 
 void limpar_buffer();
 void limpa_tela();
@@ -34,12 +37,14 @@ int main()
 {
 	setlocale (LC_ALL,"");
 	
-	int i,definicao_jogador=0, ganhou=0,numero_jogadas=0, jogada_valida, exibicao=1;
+	int i,definicao_jogador=0, ganhou=0,numero_jogadas=0, jogada_valida;
 	dados_jogadores jogadores[2];
+	int tabuleiro_posi[3][3];
 	char posicoes_tabuleiro[3][3];
 	int posicao;
+	int* posi=NULL;
 	
-	definicao_posicoes(posicoes_tabuleiro);	
+	definicao_posicoes(tabuleiro_posi);	
 	
 	cadastro_jogadores (jogadores);
 
@@ -51,32 +56,33 @@ int main()
 	{
 		
 		limpa_tela();
-		printf ("\t\t\t\t\t\t    Vez de %s\n\n\n\n\n\n",jogadores[definicao_jogador].nome);
-		posicao = tabuleiro(posicoes_tabuleiro,exibicao);	
+		printf ("\t\t\t\t\t\t    Vez de %s\n\n\n",jogadores[definicao_jogador].nome);
+		tabuleiro(tabuleiro_posi);
+		posicao = seleciona_posicao();
+		posi = 	posicao_tabuleiro(posicao);	
 		
-		jogada_valida = jogada(posicoes_tabuleiro, posicao,jogadores,definicao_jogador);
+		jogada_valida = jogada(tabuleiro_posi, posi,jogadores,definicao_jogador);
+		preencher_tabuleiro_vizualizacao(tabuleiro_posi, posicoes_tabuleiro);
 		
-		// loop que será executado enquanto o usuário não digita uma opção valida 
+		// loop executarÃ¡ enquanto o jogador nÃ£o digitar uma posiÃ§Ã£o valida 
 		while(jogada_valida != 1)
 		{
-			
+			printf ("\n\t\t\t\t\t\t POSIÃ‡ÃƒO INVALIDA!\n");
 			limpa_tela();
-			printf ("\t\t\t\t\t\t    Vez de %s\n\n\n\n\n\n",jogadores[definicao_jogador].nome);
-			posicao = tabuleiro(posicoes_tabuleiro,exibicao);	
+			printf ("\t\t\t\t\t\t    Vez de %s\n\n\n",jogadores[definicao_jogador].nome);
+			tabuleiro(tabuleiro_posi);
+			posicao = seleciona_posicao();
+			posi = 	posicao_tabuleiro(posicao);
 			
-			jogada_valida = jogada(posicoes_tabuleiro, posicao,jogadores,definicao_jogador);
+			jogada_valida = jogada(tabuleiro_posi, posi,jogadores,definicao_jogador);
+			preencher_tabuleiro_vizualizacao(tabuleiro_posi, posicoes_tabuleiro);
 		}	
-		
-		if (jogada_valida == 1)
-		{
 			
-			numero_jogadas++;
+		numero_jogadas++;
 			
-		}
+		ganhou = verificacao_gahador(tabuleiro_posi);
 		
-		ganhou = verificacao_gahador(posicoes_tabuleiro);
-		
-		// Teste que indicará a vez dos jogadores 
+		// Teste que indicar a vez dos jogadores 
 		if (definicao_jogador == 0 && ganhou != 1)
 		{
 				
@@ -91,21 +97,17 @@ int main()
 		limpa_tela();
 		
 	}
-	
-	
+		
 	if (ganhou == 1)
 	{
-		printf ("\n\n\n\t\t\t\t\t\t   PARABÉNS %s\n\n", jogadores[definicao_jogador].nome);
-		exibicao = 0;
-		tabuleiro(posicoes_tabuleiro,exibicao);
+		printf ("\n\n\n\t\t\t\t\t\t   PARABÃ‰NS %s\n\n", jogadores[definicao_jogador].nome);
+		tabuleiro(tabuleiro_posi);
 			
 	}else 
 	{
 		
 		printf ("\n\n\n\t\t\t\t\t\t       EMPATE!!\n\n\n");
-		exibicao = 0;
-		tabuleiro(posicoes_tabuleiro,exibicao);
-		
+		tabuleiro(tabuleiro_posi);	
 	}
 
 	pause();
@@ -113,50 +115,77 @@ int main()
 	return 0;
 }
 
-// Função que armazena os números relativos a posição de jogada no tabuleiro 
-void definicao_posicoes(char posicoes[3][3])
+// Declara todas as posiÃ§Ãµes do tabuleiro com 0 (0 = livres)
+void definicao_posicoes(int posicoes[3][3])
 {
-	  
-	int i, j,k=0;
 	
-	for (i=0;i<3;i++)
+	for (int i=0;i<3;i++)
 	{
-		for (j=0;j<3;j++)
-		{
-			k++;
-			
-			// k+'0' transforma o número inteiro num char
-			posicoes[i][j] = k+'0'; 
-			
+		for (int j=0;j<3;j++)
+		{ 
+			posicoes[i][j] = 0;
 		}
-	}
+	}	
 }
 
 // Procediemnto que cadastra os jogadores da partida 
 void cadastro_jogadores(dados_jogadores *jogadores)
 {
 	
-	int i;
+	(jogadores + 0)->simbolo = 1;
+	(jogadores + 1)->simbolo = 2;
 	
-	(jogadores + 0)->simbolo = 'x';
-	(jogadores + 1)->simbolo = 'o';
-	
-	for (i=0; i<2; i++)
+	for (int i=0; i<2; i++)
 	{
 		
-		printf ("Digite o nome do %d° jogador: ",i+1);
+		printf ("Digite o nome do %dÂ° jogador: ",i+1);
 		fgets ((jogadores + i)->nome,20,stdin);
-		limpar_buffer();
 		
 	}
 }
 
-// Função que exibe o tabuleiro para o usuário e retorna a posição digitada
-int tabuleiro(char posicoes[3][3], int exibicao_necessaria)
+// preenche a matriz com as teclas para selecionar a posiÃ§Ã£o ou o simbolo de um jogador
+void preencher_tabuleiro_vizualizacao(int posicoes[3][3], char vizualizacao[3][3])
+{	
+
+	int k=0;
+
+	for (int i =0; i < 3; i++)
+	{
+		for (int j =0; j < 3; j++)
+		{
+			k++;
+			vizualizacao[i][j] = '0'+k;
+		}
+	}
+
+	for (int i =0; i < 3; i++)
+	{
+		for (int j =0; j < 3; j++)
+		{	
+			if (posicoes[i][j] == 1)
+			{
+				vizualizacao[i][j] = 'x';
+
+			}
+			else if (posicoes[i][j] == 2)
+			{
+
+				vizualizacao[i][j] = 'o';
+
+			}	
+		}
+	}
+
+}
+
+// Exibe o tabuleiro para o usuÃ¡rio
+void tabuleiro(int tabuleiro_posi[3][3])
 {
+
+	char posicoes[3][3];
 	
-	int jogada;
-	
+	preencher_tabuleiro_vizualizacao(tabuleiro_posi, posicoes);
 	
 	printf ("\t\t\t\t\t          |              |           \n");
 	printf ("\t\t\t\t\t     %c    |       %c      |	%c   \n",posicoes[0][0],posicoes[0][1],posicoes[0][2]);
@@ -171,103 +200,114 @@ int tabuleiro(char posicoes[3][3], int exibicao_necessaria)
 	printf ("\t\t\t\t\t     %c    |       %c      |	%c   \n",posicoes[2][0],posicoes[2][1],posicoes[2][2]);
 	printf ("\t\t\t\t\t          |              |           \n");
 	
-	// Teste condicional parar saber se a impressão é necessaria
-	if (exibicao_necessaria == 1)
-	{
-		
-		printf ("\n\n\nDigite em qual casa você quer colocar a peça: ");
-		scanf ("%d",&jogada);
-		limpar_buffer();
-		
-	}
-	
+}
+
+// Captura a posiÃ§Ã£o digitada e a retorna
+// returna o numero exibido na tela refernte a posiÃ§Ã£o escolhida
+int seleciona_posicao()
+{
+	int jogada;
+
+	printf ("\n\n\nDigite em qual casa vocÃª quer colocar a peÃ§a: ");
+	scanf ("%d",&jogada);
+	limpar_buffer();
+
 	return jogada;
 }
 
-// Função que salva o simbolo conrespoindente ao usuário (x ou o)
-// na matriz na posicão digitada pelo usuário
-// OBS: a função retorna se a posição digita é valida ou não 
-int jogada(char posicoes[3][3], int posicao_jogada,dados_jogadores *dados,int jogador)
+// Seta a posicao i e j num vetor
+// returna um ponteiro para inteiro
+int* posicao_tabuleiro(int posicao)
 {
-	
-	int opcao_valida, teste;
-	
-	// Camamento da função para saber se na posição digitada já não está com um simbolo dentro 
-	teste = posicao_valida(posicoes,posicao_jogada);
-	
-	// teste para saber se a posição é valida
-	// se não for ela atribuirá 10 a posição_jogada 
-	// fazendo cair no default
-	if (teste == 0)
-	{
-		
-		posicao_jogada = 10;
-		
-	}
-	
-	switch (posicao_jogada)
+	int* posi = calloc(2,sizeof(posi));
+
+	switch (posicao)
 	{
 		
 		case 1:
 			
-			posicoes[0][0] = (dados +jogador)->simbolo;
-			opcao_valida = 1; 
+			*(posi + 0) = 0;
+			*(posi + 1) = 0;
 			break;
 			
 		case 2:
 			
-			posicoes[0][1] = (dados +jogador)->simbolo;
-			opcao_valida = 1;
+			*(posi + 0) = 0;
+			*(posi + 1) = 1;
 			break;
 		
 		case 3:
 			
-			posicoes[0][2] = (dados +jogador)->simbolo;
-			opcao_valida = 1;
+			*(posi + 0) = 0;
+			*(posi + 1) = 2;
 			break;
 		
 		case 4:
 			
-			posicoes[1][0] = (dados +jogador)->simbolo;
-			opcao_valida = 1;
+			*(posi + 0) = 1;
+			*(posi + 1) = 0;
 			break;
 		
 		case 5:
 			
-			posicoes[1][1] = (dados +jogador)->simbolo;
-			opcao_valida = 1;
+			*(posi + 0) = 1;
+			*(posi + 1) = 1;
 			break;
 		
 		case 6:
 			
-			posicoes[1][2] = (dados +jogador)->simbolo;
-			opcao_valida = 1;
+			*(posi + 0) = 1;
+			*(posi + 1) = 2;
 			break;
 		
 		case 7:
 			
-			posicoes[2][0] = (dados +jogador)->simbolo;
-			opcao_valida = 1;
+			*(posi + 0) = 2;
+			*(posi + 1) = 0;
 			break;
 		
 		case 8:
 			
-			posicoes[2][1] = (dados +jogador)->simbolo;
-			opcao_valida = 1;
+			*(posi + 0) = 2;
+			*(posi + 1) = 1;
 			break;
 		
 		case 9:
 			
-			posicoes[2][2] = (dados +jogador)->simbolo;
-			opcao_valida = 1;
+			*(posi + 0) = 2;
+			*(posi + 1) = 2;
 			break;
 		
 		default:
 			
-			fflush (stdout);
-			printf ("\n\n\t\t\t\t\t\t   POSIÇÃO INVÁLIDA!\n\n");
-			opcao_valida = 0;
+			printf("\n\n\t\tPosicao invalida!!");
 			break;	
+	}
+
+	return posi;
+
+}
+
+// Salva o simbolo conrespoindente ao usuÃ¡rio (x ou o)
+// na matriz na posiÃ§Ã£o digitada pelo usuÃ¡rio
+// OBS: a funÃ§Ã£oo retorna se a posiÃ§Ã£o digita Ã© valida ou nÃ£o 
+int jogada(int posicoes[3][3], int* posicao_jogada,dados_jogadores *dados,int jogador)
+{
+	
+	int opcao_valida, i = *(posicao_jogada + 0), j = *(posicao_jogada + 1), valida;
+	
+	// teste para saber se a posiÃ§Ã£o Ã© valida
+	// se nÃ£o for ela atribuir 10 a posicao_jogada 
+	if (posicao_valida(posicoes,posicao_jogada) == 1)
+	{
+		posicoes[i][j] = (dados + jogador)->simbolo;
+		opcao_valida = 1;
+		
+	}
+	else
+	{
+		opcao_valida = 0;
+
 	}
 	
 	pause ();
@@ -275,158 +315,69 @@ int jogada(char posicoes[3][3], int posicao_jogada,dados_jogadores *dados,int jo
 	return opcao_valida;
 }
 
-// Função para saber se na posição digitada já não está com um simbolo dentro
-// Retornará 1 se a posição for valida e 0 se for invalida
-int posicao_valida(char posicoes[3][3], int posi)
+// Saber se na posiÃ§Ã£o digitada jÃ¡  estÃ¡ com um simbolo dentro
+// Retornar 1 se a posiÃ§Ã£o for valida e 0 se for invalida
+int posicao_valida(int posicoes[3][3], int* posi)
 {
 	
-	int valida=1;
+	int i = *(posi + 0), j = *(posi + 1), valida;
 	
-	switch (posi)
+	if (posicoes[i][j] == 0)
 	{
-		
-		case 1:
-			
-			if (posicoes[0][0] == 'x' || posicoes[0][0] == 'o')
-			{
-				
-				valida = 0;	
-				
-			} 
-			break;
-			
-		case 2:
-			
-			if (posicoes[0][1] == 'x' || posicoes[0][1] == 'o')
-			{
-				
-				valida = 0;	
-				
-			} 
-			break;
-		
-		case 3:
-			
-			if (posicoes[0][2] == 'x' || posicoes[0][2] == 'o')
-			{
-				
-				valida = 0;	
-				
-			} 
-			break;
-		
-		case 4:
-			
-			if (posicoes[1][0] == 'x' || posicoes[1][0] == 'o')
-			{
-				
-				valida = 0;	
-				
-			}
-			break;
-		
-		case 5:
-			
-			if (posicoes[1][1] == 'x' || posicoes[1][1] == 'o')
-			{
-				
-				valida = 0;	
-			
-			}
-			break;
-		
-		case 6:
-			
-			if (posicoes[1][2] == 'x' || posicoes[1][2] == 'o')
-			{
-				
-				valida = 0;	
-			
-			}
-			break;
-		
-		case 7:
-			
-			if (posicoes[2][0] == 'x' || posicoes[2][0] == 'o')
-			{
-				
-				valida = 0;	
-			
-			}
-			break;
-		
-		case 8:
-			
-			if (posicoes[2][1] == 'x' || posicoes[2][1] == 'o')
-			{
-				
-				valida = 0;	
-			
-			}
-			break;
-		
-		case 9:
-			
-			if (posicoes[2][2] == 'x' || posicoes[2][2] == 'o')
-			{
-				
-				valida = 0;	
-			
-			}
-			break;
+		valida = 1;
+	}
+	else
+	{
+		valida = 0;
+
 	}
 	
 	return valida;
 	
 }
 
-// Função que verifica se existe algum ganhador
-// Retornará 0 se não houver ganhador e 1 se houver 
-int verificacao_gahador(char posicoes[3][3])
+// Verifica se existe algum ganhador
+// Retornar 0 se nÃ£o houver ganhador e 1 se houver 
+int verificacao_gahador(int posicoes[3][3])
 {
 	
-	int i,ganhou=0;
+	int ganhou=0;
 	
-	for (i=0; i<3; i++)
+	for (int i=0; i<3; i++)
 	{
-		if ((posicoes[i][0] == posicoes[i][1]) && (posicoes[i][1] == posicoes[i][2]))
+		if (((posicoes[i][0] == posicoes[i][1]) && (posicoes[i][1] == posicoes[i][2])) && (posicoes[i][0] != 0))
 		{
 			
-			ganhou = 1;
-			
+			ganhou = 1;	
 		}
 	}
 	
-	for (i=0; i<3; i++)
+	for (int i=0; i<3; i++)
 	{
-		if ((posicoes[0][i] == posicoes[1][i]) && (posicoes[1][i] == posicoes[2][i]))
+		if ((posicoes[0][i] == posicoes[1][i]) && (posicoes[1][i] == posicoes[2][i]) && (posicoes[0][i] != 0))
 		{
 			
-			ganhou = 1;
-			
+			ganhou = 1;	
 		}
 	}
 	
-	if ((posicoes[0][0] == posicoes[1][1]) && (posicoes[1][1] == posicoes[2][2]))
+	if ((posicoes[0][0] == posicoes[1][1]) && (posicoes[1][1] == posicoes[2][2]) && (posicoes[0][0] != 0))
 	{
 			
-		ganhou = 1;
-			
+		ganhou = 1;	
 	}
 	
-	if ((posicoes[0][2] == posicoes[1][1]) && (posicoes[1][1] == posicoes[2][0]))
+	if ((posicoes[0][2] == posicoes[1][1]) && (posicoes[1][1] == posicoes[2][0]) && (posicoes[0][2] != 0))
 	{
 			
-		ganhou = 1;
-			
+		ganhou = 1;		
 	}
-	
 
 	return ganhou;
 	
 }
 
-// Função para limpar o buffer do teclado
+// Limpa o buffer do teclado
 void limpar_buffer()
 {
 	
@@ -436,16 +387,16 @@ void limpar_buffer()
 	
 }
 
-// Função que simula uma limpase de tela
+// Simula uma limpase de tela
 void limpa_tela()
 {
 
-	printf ("\n\n\n\n\n\n");
+	printf ("\n\n\n\n");
 	
 	
 }
 
-// Função que pausa o sistema até o usuário digitar uma tecla
+// Pausa o sistema atÃ© o usuÃ¡rio digitar uma tecla
 void pause ()
 {
 	
